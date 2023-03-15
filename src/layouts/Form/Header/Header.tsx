23fbc8bb1101a37b07e26e4a7e030c8f18d8aaa4
mojsx <mojsx.dev@gmail.com>
2023-03-15 14:26:17 +0100

    feat: add Form Header.

--- src/layouts/Form/Header/Header.tsx
+++ src/layouts/Form/Header/Header.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import "./header.css";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className="header">
+            <img src={"https://picsum.photo/200/300/seed"} alt={"Haha!"} />
+            <h1>File Name</h1>
+        </header>
+    );
 };
 
 export default Header;
