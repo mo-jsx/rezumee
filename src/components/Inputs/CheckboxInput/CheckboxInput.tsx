a3974e4a8b137218e7d09a9c65ea14fb9422062e
mojsx <mojsx.dev@gmail.com>
2023-03-15 14:32:09 +0100

    feat: add Inputs components.

---
+++ src/components/Inputs/CheckboxInput/CheckboxInput.tsx
@@ -0,0 +1,20 @@
+import { useField } from "formik";
+
+const CheckboxInput = ({ children, ...props }) => {
+    const [field, meta] = useField({ ...props, type: "checkbox" });
+
+    return (
+        <div>
+            <label className="checkbox-input">
+                <input type="checkbox" {...field} {...props} />
+                {children}
+            </label>
+
+            {meta.touched && meta.error ? (
+                <div className="erro">{meta.error}</div>
+            ) : null}
+        </div>
+    );
+};
+
+export default CheckboxInput;
---
+++ src/components/Inputs/index.tsx
@@ -0,0 +1,3 @@
+export { default as Checkbox } from "./CheckboxInput";
+export { default as Select } from "./SelectInput";
+export { default as Text } from "./TextInput";
