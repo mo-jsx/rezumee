69c2e20da7903b384f9b85d59ba8375e89fdfa2a
mojsx <mojsx.dev@gmail.com>
2023-03-24 15:54:00 +0100

    types: add user details types.

---
+++ src/types/Details.ts
@@ -0,0 +1,10 @@
+interface Details {
+    firstname: string;
+    lastname: string;
+    email: string;
+    phone: string;
+    address: string;
+    bio: string;
+}
+
+export default Details;
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { default as Details } from "./Details";
