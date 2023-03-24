ae00c3abb417dfb3c205714a0b60c31d99125451
mojsx <mojsx.dev@gmail.com>
2023-03-24 15:55:00 +0100

    component: fix image placeholder url.

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -3,7 +3,7 @@ import "./header.css";
 const Header = () => {
     return (
         <header className="header">
-            <img src={"https://picsum.photo/200/300/seed"} alt={"Haha!"} />
+            <img src={"https://picsum.photos/200/200"} alt={"Haha!"} />
             <h1>File Name</h1>
         </header>
     );
