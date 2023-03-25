66a7f8480fbb0d1fd7624267f4fffc3e77030027
mojsx <mojsx.dev@gmail.com>
2023-03-25 14:21:12 +0100

    store: update properties, setters.

--- src/store/profileStore.tsx
+++ src/store/profileStore.tsx
@@ -1,18 +1,38 @@
 import { create } from "zustand";
-
 import { Details } from "types/";
 
 interface ProfileState extends Details {
-    update: (prop: string, by: string) => void;
+    updateFirstName: (by: string) => void;
+    updateMiddleName: (by: string) => void;
+    updateLastName: (by: string) => void;
+    updateJob: (by: string) => void;
+    updateEmail: (by: string) => void;
+    updatePhone: (by: string) => void;
+    updateAddress: (by: string) => void;
+    updateWebsite: (by: string) => void;
+    updateBio: (by: string) => void;
 }
 
 const useProfileStore = create<ProfileState>()(set => ({
-    firstname: "",
-    lastname: "",
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    job: "",
     email: "",
     phone: "",
     address: "",
+    website: "",
     bio: "",
 
-    update: by => set({ firstname: by }),
+    updateFirstName: firstName => set({ firstName: firstName }),
+    updateMiddleName: middleName => set({ middleName: middleName }),
+    updateLastName: lastName => set({ lastName: lastName }),
+    updateJob: job => set({ job: job }),
+    updateEmail: email => set({ email: email }),
+    updatePhone: phone => set({ phone: phone }),
+    updateAddress: address => set({ address: address }),
+    updateWebsite: website => set({ website: website }),
+    updateBio: bio => set({ bio: bio }),
 }));
+
+export default useProfileStore;
