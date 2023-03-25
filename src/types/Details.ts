43d48a0731abb8694a07a0ac0be209df16d8ddec
mojsx <mojsx.dev@gmail.com>
2023-03-25 14:20:03 +0100

    types: add firstName, middleName, job types.

--- src/types/Details.ts
+++ src/types/Details.ts
@@ -1,9 +1,12 @@
 interface Details {
-    firstname: string;
-    lastname: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    job: string;
     email: string;
     phone: string;
     address: string;
+    website: string;
     bio: string;
 }
 
