38112b18d34b99b1877a164a5be461f34968b2d6
mojsx <mojsx.dev@gmail.com>
2023-03-25 14:18:36 +0100

    feat: add update file name from store firstName.

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,10 +1,19 @@
+import useProfileStore from "../../store/profileStore";
 import "./header.css";
 
 const Header = () => {
+    const { firstName } = useProfileStore(state => ({
+        firstName: state.firstName,
+    }));
+
     return (
         <header className="header">
             <img src={"https://picsum.photos/200/200"} alt={"Haha!"} />
-            <h1>File Name</h1>
+            {firstName ? (
+                <h1>{firstName.toUpperCase()}'s resume</h1>
+            ) : (
+                <h1>Resume</h1>
+            )}
         </header>
     );
 };
