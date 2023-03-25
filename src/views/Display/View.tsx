d6f68f1e2143ba9b0ac3a455ff5ab3e2b9c3f950
mojsx <mojsx.dev@gmail.com>
2023-03-25 14:22:08 +0100

    chores: display firstName from store.

--- src/views/Display/View.tsx
+++ src/views/Display/View.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import "./view.scss";
+import useProfileStore from "../../store/profileStore";
 
 const View = () => {
-    return <div>View</div>;
+    const { firstName } = useProfileStore(state => ({
+        firstName: state.firstName,
+    }));
+
+    return (
+        <div className="view">
+            View
+            <h1>Here's : {firstName}</h1>
+        </div>
+    );
 };
 
 export default View;
