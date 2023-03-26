69566f58ac41ea886731d5a3f2f23fdd5891872e
mojsx <mojsx.dev@gmail.com>
2023-03-26 15:44:11 +0200

    feat: add fields placeholders.

--- src/views/Display/View.tsx
+++ src/views/Display/View.tsx
@@ -24,7 +24,12 @@ const View = () => {
         address: state.address,
     }));
 
-    const contact = [email, phone, address, website];
+    const contact = [
+        { data: email, placeholder: "example@email.com" },
+        { data: phone, placeholder: "(+1) 555 555 555" },
+        { data: address, placeholder: "Newark, New Jersey" },
+        { data: website, placeholder: "www.tony-soprano.com" },
+    ];
 
     return (
         <div className="view">
@@ -35,16 +40,26 @@ const View = () => {
                     </div>
                     <div className="header-name">
                         <h2>
-                            {firstName} {middleName.substring(0, 1)}. {lastName}
+                            {firstName ? firstName : "Firstname"}{" "}
+                            {middleName ? `${middleName.substring(0, 1)}.` : ""}{" "}
+                            {lastName ? lastName : "Lastname"}
                         </h2>
-                        <h4>{job}</h4>
-                        <p>{bio}</p>
+                        <h4>{job ? job : "General Manager"}</h4>
+                        <p>
+                            {bio
+                                ? bio
+                                : "A curios individual, who wants to master his craft."}
+                        </p>
                     </div>
 
                     <div className="header-contact">
                         <ul>
                             {contact.map((field, index) => (
-                                <li key={index}>{field}</li>
+                                <li key={index}>
+                                    {field.data
+                                        ? field.data
+                                        : field.placeholder}
+                                </li>
                             ))}
                         </ul>
                     </div>
