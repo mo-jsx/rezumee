3688f282868867d93922a24b44b2e20faf3fd839
mojsx <mojsx.dev@gmail.com>
2023-04-02 13:51:08 +0200

    store: add properties update on value change.

--- src/layouts/Forms/Experience/Experience.tsx
+++ src/layouts/Forms/Experience/Experience.tsx
@@ -1,8 +1,36 @@
 import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
 import { GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
+import useProfileStore from "../../../store/profileStore";
 import "./styles.scss";
+import React from "react";
 
 const Experience = () => {
+    const { jobTitle, comapny, start, end, location, jobDesc } =
+        useProfileStore(state => ({
+            jobTitle: state.jobTitle,
+            comapny: state.company,
+            start: state.start,
+            end: state.end,
+            location: state.location,
+            jobDesc: state.jobDesc,
+        }));
+
+    const {
+        updateJobTitle,
+        updateCompany,
+        updateStart,
+        updateEnd,
+        updateLocation,
+        updateJobDesc,
+    } = useProfileStore(state => ({
+        updateJobTitle: state.updateJobTitle,
+        updateCompany: state.updateCompany,
+        updateStart: state.updateStart,
+        updateEnd: state.updateEnd,
+        updateLocation: state.updateLocation,
+        updateJobDesc: state.updateJobDesc,
+    }));
+
     const initValues = {
         job: "",
         company: "",
@@ -11,6 +39,7 @@ const Experience = () => {
         location: "",
         desc: "",
     };
+
     return (
         <div>
             <Formik
@@ -20,81 +49,123 @@ const Experience = () => {
                     actions.setSubmitting(false);
                 }}
             >
-                <Form className="experience-form">
-                    <h2>Your Experience</h2>
-                    <div className="experience">
-                        <div className="details-input jobTitle">
-                            <label htmlFor="job">Job Title</label>
-                            <Field
-                                name="job"
-                                id="job"
-                                placeholder="CEO"
-                                className="input"
-                            />
-                            <ErrorMessage name="job" />
-                        </div>
+                {formik => {
+                    return (
+                        <Form className="experience-form">
+                            <h2>Your Experience</h2>
+                            <div className="experience">
+                                <div className="details-input jobTitle">
+                                    <label htmlFor="job">Job Title</label>
+                                    <Field
+                                        name="job"
+                                        id="job"
+                                        placeholder="CEO"
+                                        className="input"
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLSelectElement>,
+                                        ) => {
+                                            formik.handleChange(e);
+                                            updateJobTitle(e.target.value);
+                                        }}
+                                    />
+                                    <ErrorMessage name="job" />
+                                </div>
 
-                        <div className="details-input company">
-                            <label htmlFor="company">Company</label>
-                            <Field
-                                name="company"
-                                id="company"
-                                placeholder="CEO"
-                                className="input"
-                            />
-                            <ErrorMessage name="company" />
-                        </div>
+                                <div className="details-input company">
+                                    <label htmlFor="company">Company</label>
+                                    <Field
+                                        name="company"
+                                        id="company"
+                                        placeholder="CEO"
+                                        className="input"
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLSelectElement>,
+                                        ) => {
+                                            formik.handleChange(e);
+                                            updateCompany(e.target.value);
+                                        }}
+                                    />
+                                    <ErrorMessage name="company" />
+                                </div>
 
-                        <div className="details-input startDate">
-                            <label htmlFor="start">Start</label>
-                            <Field
-                                type="date"
-                                name="start"
-                                id="start"
-                                placeholder="CEO"
-                                className="input"
-                            />
-                            <ErrorMessage name="start" />
-                        </div>
+                                <div className="details-input startDate">
+                                    <label htmlFor="start">Start</label>
+                                    <Field
+                                        type="date"
+                                        name="start"
+                                        id="start"
+                                        placeholder="CEO"
+                                        className="input"
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLSelectElement>,
+                                        ) => {
+                                            formik.handleChange(e);
+                                            updateStart(
+                                                new Date(e.target.value),
+                                            );
+                                        }}
+                                    />
+                                    <ErrorMessage name="start" />
+                                </div>
 
-                        <div className="details-input endDate">
-                            <label htmlFor="end">End</label>
-                            <Field
-                                type="date"
-                                name="end"
-                                id="end"
-                                placeholder="CEO"
-                                className="input"
-                            />
-                            <ErrorMessage name="end" />
-                        </div>
+                                <div className="details-input endDate">
+                                    <label htmlFor="end">End</label>
+                                    <Field
+                                        type="date"
+                                        name="end"
+                                        id="end"
+                                        placeholder="CEO"
+                                        className="input"
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLSelectElement>,
+                                        ) => {
+                                            formik.handleChange(e);
+                                            updateEnd(new Date(e.target.value));
+                                        }}
+                                    />
+                                    <ErrorMessage name="end" />
+                                </div>
 
-                        <div className="details-input location">
-                            <label htmlFor="location">Location</label>
-                            <Field
-                                name="location"
-                                id="location"
-                                placeholder="CEO"
-                                className="input"
-                            />
-                            <ErrorMessage name="location" />
-                        </div>
+                                <div className="details-input location">
+                                    <label htmlFor="location">Location</label>
+                                    <Field
+                                        name="location"
+                                        id="location"
+                                        placeholder="CEO"
+                                        className="input"
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLSelectElement>,
+                                        ) => {
+                                            formik.handleChange(e);
+                                            updateLocation(e.target.value);
+                                        }}
+                                    />
+                                    <ErrorMessage name="location" />
+                                </div>
 
-                        <div className="details-input jobDesc">
-                            <label htmlFor="desc">Description</label>
-                            <GrammarlyEditorPlugin clientId="client_8uvgmmYtA3FKgst7xf8ZfF">
-                                <Field
-                                    as="textarea"
-                                    name="desc"
-                                    id="desc"
-                                    placeholder="CEO"
-                                    className="textarea"
-                                />
-                            </GrammarlyEditorPlugin>
-                            <ErrorMessage name="desc" />
-                        </div>
-                    </div>
-                </Form>
+                                <div className="details-input jobDesc">
+                                    <label htmlFor="desc">Description</label>
+                                    <GrammarlyEditorPlugin clientId="client_8uvgmmYtA3FKgst7xf8ZfF">
+                                        <Field
+                                            as="textarea"
+                                            name="desc"
+                                            id="desc"
+                                            placeholder="CEO"
+                                            className="textarea"
+                                            onChange={(
+                                                e: React.ChangeEvent<HTMLSelectElement>,
+                                            ) => {
+                                                formik.handleChange(e);
+                                                updateJobDesc(e.target.value);
+                                            }}
+                                        />
+                                    </GrammarlyEditorPlugin>
+                                    <ErrorMessage name="desc" />
+                                </div>
+                            </div>
+                        </Form>
+                    );
+                }}
             </Formik>
         </div>
     );
