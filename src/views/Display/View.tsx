1a18ab7201dc3d50dc91e41ac6213f0e33dcf231
mojsx <mojsx.dev@gmail.com>
2023-04-02 13:52:37 +0200

    feat: add Header component.

--- src/views/Display/View.tsx
+++ src/views/Display/View.tsx
@@ -1,69 +1,12 @@
 import "./view.scss";
 import useProfileStore from "../../store/profileStore";
+import Header from "../../layouts/Display/Header";
 
 const View = () => {
-    const {
-        firstName,
-        middleName,
-        lastName,
-        email,
-        phone,
-        website,
-        address,
-        job,
-        bio,
-    } = useProfileStore(state => ({
-        firstName: state.firstName,
-        middleName: state.middleName,
-        lastName: state.lastName,
-        email: state.email,
-        phone: state.phone,
-        website: state.website,
-        job: state.job,
-        bio: state.bio,
-        address: state.address,
-    }));
-
-    const contact = [
-        { data: email, placeholder: "example@email.com" },
-        { data: phone, placeholder: "(+1) 555 555 555" },
-        { data: address, placeholder: "Newark, New Jersey" },
-        { data: website, placeholder: "www.tony-soprano.com" },
-    ];
-
     return (
         <div className="view">
             <div className="sheet">
-                <div className="view-header">
-                    <div className="header-img">
-                        <img src="https://picsum.photos/200/200" alt="Haha!" />
-                    </div>
-                    <div className="header-name">
-                        <h2>
-                            {firstName ? firstName : "Firstname"}{" "}
-                            {middleName ? `${middleName.substring(0, 1)}.` : ""}{" "}
-                            {lastName ? lastName : "Lastname"}
-                        </h2>
-                        <h4>{job ? job : "General Manager"}</h4>
-                        <p>
-                            {bio
-                                ? bio
-                                : "A curios individual, who wants to master his craft."}
-                        </p>
-                    </div>
-
-                    <div className="header-contact">
-                        <ul>
-                            {contact.map((field, index) => (
-                                <li key={index}>
-                                    {field.data
-                                        ? field.data
-                                        : field.placeholder}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
+                <Header />
             </div>
         </div>
     );
