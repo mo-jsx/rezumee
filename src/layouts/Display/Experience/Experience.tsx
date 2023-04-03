c8edde8165595650d0434b0a1b7e7455e0280b3f
mojsx <mojsx.dev@gmail.com>
2023-04-03 18:39:35 +0200

    layout: add Display Experience layout.

---
+++ src/layouts/Display/Experience/Experience.tsx
@@ -0,0 +1,60 @@
+import moment from "moment";
+import useProfileStore from "../../../store/profileStore";
+import "./styles.scss";
+
+const DescPlaceholder = () => {
+    return (
+        <ul>
+            <li>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Libero, doloremque.
+            </li>
+        </ul>
+    );
+};
+
+const Experience = () => {
+    const { jobTitle, company, start, end, location, jobDesc } =
+        useProfileStore(state => ({
+            jobTitle: state.jobTitle,
+            company: state.company,
+            start: state.start,
+            end: state.end,
+            location: state.location,
+            jobDesc: state.jobDesc,
+        }));
+
+    return (
+        <div className="display-experience">
+            <h2>Experience</h2>
+            <div className="head">
+                <div className="left">
+                    <h3 className="job-title">
+                        {jobTitle ? jobTitle : "Job Title"},{" "}
+                        <span className="heading-regular">
+                            {company ? company : "Company Name"}
+                        </span>
+                    </h3>
+                    <h5 className="heading-regular no-padding-no-margin">
+                        {start
+                            ? moment(start).format("MMMM YYYY")
+                            : "Start date"}{" "}
+                        - {end ? moment(end).format("MMMM YYYY") : "Present"}
+                    </h5>
+                </div>
+
+                <div className="right">
+                    <h4 className="heading-regular">
+                        {location ? location : "Location"}
+                    </h4>
+                </div>
+            </div>
+
+            <div className="desc">
+                {jobDesc ? jobDesc : <DescPlaceholder />}
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
