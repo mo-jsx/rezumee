8272f8e0a3214e7fea8888633d776010f63f97ce
mojsx <mojsx.dev@gmail.com>
2023-04-06 23:46:42 +0200

    feat: add pdf print.

--- src/views/Display/View.tsx
+++ src/views/Display/View.tsx
@@ -1,14 +1,31 @@
+import { useRef } from "react";
+import jsPDF from "jspdf";
 import "./view.scss";
 import { Education, Experience, Header } from "../../layouts/Display";
 
 const View = () => {
+    const pdfRef: any = useRef(null);
+
+    const generatePdf = () => {
+        const pdf = new jsPDF("p", "pt", "letter");
+
+        pdf.html(pdfRef.current, {
+            callback: () => {
+                pdf.save("my-rezumee.pdf");
+            },
+        });
+    };
+
     return (
         <div className="view">
-            <div className="sheet">
+            <div className="sheet" ref={pdfRef}>
                 <Header />
                 <Experience />
                 <Education />
             </div>
+            <button className="print-button" onClick={generatePdf}>
+                Print
+            </button>
         </div>
     );
 };
